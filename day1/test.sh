553b80536de2f5a630e0ddc7d0d7ea8221f683e9
Ersin OZTURK <ersnozturkk@gmail.com>
2026-02-25 20:27:59 +0300

    Product & Order Service

--- day1/test.sh
+++ day1/test.sh
@@ -1,4 +1,4 @@
 
-( seq 1 10000 | xargs -P 5 -I {} curl http://localhost:8081/js_container2/ ) &
-( seq 1 10000 | xargs -P 5 -I {} curl http://localhost:8081/js_container/ ) &
-wait
+seq 1 10000 | xargs -P 1000 -I {} curl http://localhost:8081/order/
+
+seq 1 10000 | xargs -P 1000 -I {} curl http://localhost:8081/product/
